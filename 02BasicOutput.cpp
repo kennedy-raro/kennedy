ec0bd77a4b9bdc4904094b2c2c998dcb9077dd3f
Kenn <kennedyraro79@gmail.com>
2024-10-08 09:29:54 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	cout << "OUR LADY OF LOURDES COLLEGE FOUNDATION" << endl;
+	cout << "COMPUTER STUDIES" << endl;
+	cout << "BSIT 1st Year BLOCK A" << endl;
+	cout << "JOHN KENNEDY C. RARO" << endl;
+	cout << "P-4A TULAY NA LUPA, LABO, CAMARINES NORTE" << endl;
+	
+	return 0;
+}
