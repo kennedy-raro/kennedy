59271533cee51801e0f74482a65c762a9f95b9d0
Kenn <kennedyraro79@gmail.com>
2024-10-09 09:15:27 +0800

    Add files via upload

---
+++ 05DataTypesOperator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num1, num2;
+	
+	cout << "Enter first number:";
+	cin >> num1;
+	cout << "Enter second Number:";
+	cin >> num2;
+	
+	cout << "Sum" << num1 + num2 << endl;
+	cout << "Difference" << num1 - num2 << endl;
+	cout << "Qoutient" << num1 * num2 << endl;
+	cout << "Product" << num1 / num2 << endl;
+	cout << "Remainder" << num1 % num2 << endl;
+	
+	return 0;
+}
+
+	
+	
+	
+	
